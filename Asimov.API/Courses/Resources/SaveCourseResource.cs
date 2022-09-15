d04ea2dfa79b61b22ef6f7950aca60d2890d7ee4
S-011 <u201811286@upc.edu.pe>
2022-09-15 18:19:28 -0500

    Backend upload to Git

---
+++ Asimov.API/Courses/Resources/SaveCourseResource.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asimov.API.Courses.Resources
+{
+    public class SaveCourseResource
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+        
+        [Required]
+        [MaxLength(10)]
+        public string Grade { get; set; }
+        
+        [Required]
+        public bool State { get; set; }
+    }
+}
